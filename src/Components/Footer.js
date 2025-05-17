07f38faf86055e30719c03dab079119da116f896
2309Akki <akshaysawalagi2309.gmailcom>
2025-05-17 23:13:05 +0530

    upda

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,12 +7,12 @@ const Footer = () => {
   return (
     <footer>
       <h4>Developed by Akshay </h4>
-      <h4>Copyright &copy; 2025 AS</h4>
+      <h4>Copyright &copy; 2025</h4>
       <div className='footerLinks'>
         <a href="https://github.com/2309Akki" target='_blank'><FaGithub/></a>
         <a href="www.linkedin.com/in/akshay-sawalagi" target='_blank'><FaLinkedin/></a>
-        <a href='mailTo:devanshsahni@gmail.com' target='_blank'><GrMail/></a>
-        <a href="https://leetcode.com/sahni1403/" target="_blank"><SiLeetcode/></a>
+        <a href='mailTo:akshaysawalagi2309@gmail.com' target='_blank'><GrMail/></a>
+        {/* <a href="https://leetcode.com/sahni1403/" target="_blank"><SiLeetcode/></a> */}
       </div>
     </footer>
   )
